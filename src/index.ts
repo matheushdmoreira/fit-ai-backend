3956ee585289de44163514e27ab08511652e3654
Matheus Moreira <matheustjp111@gmail.com>
2026-03-02 12:15:18 -0300

    feat:  add fastify api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-const test = 'Hello World'
+import 'dotenv/config'
+import Fastify from 'fastify'
 
-console.log(test)
+const app = Fastify({
+  logger: true,
+})
+
+app.get('/', () => {
+  return { hello: 'world' }
+})
+
+try {
+  await app.listen({ port: Number(process.env.PORT) ?? 3333 })
+} catch (err) {
+  app.log.error(err)
+  process.exit(1)
+}
